dee129c14b0a148500e4718cf0b32f9193f3f3cd
Mark <mark.barbaric@cloudhouse.com>
2020-06-01 10:06:23 +0100

    Added Linked List algorithm and testing

--- DataStructures/LinkedList/LinkedListAlgorithms.h
+++ DataStructures/LinkedList/LinkedListAlgorithms.h
@@ -5,38 +5,41 @@
 #ifndef BACKTRACKING_LINKEDLISTALGORITHMS_H
 #define BACKTRACKING_LINKEDLISTALGORITHMS_H
 
-#include "LinkedList.h"q
+#include "LinkedList.h"
 
-template<typename ListType>
-ListType* addTwoNumbers(ListType* l1, ListType* l2) {
+namespace LinkedListAlgorithms
+{
+    LinkedList* addTwoNumbers(LinkedList* l1, LinkedList* l2) {
 
-    auto* dummyHead = new ListType(0);
-    auto* cur = dummyHead;
-    auto carry = 0;
+        auto* dummyHead = new LinkedList(0);
+        auto* cur = dummyHead;
+        auto remainder = 0;
 
-    while(l1 || l2)
-    {
-        auto x1 = l1 ? l1->val : 0;
-        auto x2 = l2 ? l2->val : 0;
+        while (l1 || l2)
+        {
+            auto x1 = l1 ? l1->val : 0;
+            auto x2 = l2 ? l2->val : 0;
 
-        auto sum = carry + x1 + x2;
-        carry = sum / 10;
+            auto sum = remainder + x1 + x2;
+            remainder = sum / 10;
+            sum %= 10;
 
-        auto* newNode = new ListType(sum % 10);
-        cur->next = newNode;
-        cur = cur->next;
+            auto* newNode = new LinkedList(sum);
+            cur->next = newNode;
+            cur = cur->next;
 
-        if(l1)
-            l1 = l1->next;
-        if(l2)
-            l2 = l2->next;
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
+        }
+
+        if (remainder)
+            cur->next = new LinkedList(remainder);
+
+        return dummyHead->next;
+
     }
-
-    if(carry)
-        cur->next = new ListType(carry);
-
-    return dummyHead->next;
-
 }
 
 #endif //BACKTRACKING_LINKEDLISTALGORITHMS_H
