6baae9ddea57d5a3019d8cab0b302d6959822836
Mark Barbaric <mark.barbaric@mirriad.com>
2022-07-18 16:58:11 +0100

    Created first iteration of Circular Queue.

--- DataStructures/CircularQueues/CircularQueueTests.cpp
+++ DataStructures/CircularQueues/CircularQueueTests.cpp
@@ -1,7 +1,30 @@
-#include "include/catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.hpp"
 #include "CircularQueue.h"
 
 namespace Datastructures::CircularQueue{
+
+    TEST_CASE("Circular Queue Tests"){
+
+        CircularQueue queue1(3);
+        REQUIRE(queue1.pop() == -1);
+        REQUIRE(queue1.isEmpty() == true);
+        REQUIRE(queue1.push(12) == true);
+        REQUIRE(queue1.front() == 12);
+        REQUIRE(queue1.push(13) == true);
+        REQUIRE(queue1.front() == 13);
+        REQUIRE(queue1.push(14) == true);
+        REQUIRE(queue1.front() == 14);
+        REQUIRE(queue1.isFull() == true);
+        REQUIRE(queue1.pop() == 12);
+        REQUIRE(queue1.isFull() == false);
+        REQUIRE(queue1.push(15) == true);
+        REQUIRE(queue1.isFull() == true);
+        REQUIRE(queue1.pop() == 13);
+        REQUIRE(queue1.pop() == 14);
+        REQUIRE(queue1.pop() == 15);
+        REQUIRE(queue1.isEmpty() == true);
+    }
 
 
 }
